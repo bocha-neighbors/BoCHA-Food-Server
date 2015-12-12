30fc7ce4926e112087b1be037657cb0d624e3c3d
Ben Allred <skuttleman@gmail.com>
2015-12-12 10:30:48 -0700

    Create & Read records on catalog

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 var express = require('express');
+var db = require('./fakedata.json');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 var app = express();
 
 app.listen(8080);
 console.log('Server running on port 8080');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
   res.send('Hello World');
+});
+
+// list catalog
+app.get('/catalog', function(req, res) {
+  res.json(db.catalog);
+});
+
+app.get('/catalog/:id', function(req, res) {
+  var id = Number(req.params.id);
+  for (var i = 0; i < db.catalog.length; i++) {
+    if (db.catalog[i].id === id) {
+      var item = db.catalog[i], i = db.catalog.length;
+    }
+  }
+  if (item) res.json(item);
+  else  res.sendStatus(404);
+});
+
+app.post('/catalog/new', function(req, res) {
+  var newid = db.catalog[db.catalog.length - 1].id + 1;
+  if (req.body.name && req.body.description) {
+    db.catalog.push({ id: newid, name: req.body.name, description: req.body.description });
+    fs.writeFile('./fakedata.json', JSON.stringify(db), function(err) {
+      if (err) throw err;
+      else res.send('success');
+    });
+  } else {
+    res.send('invalid data format');
+  }
 });
