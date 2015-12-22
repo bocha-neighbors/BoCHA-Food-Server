c2168c3fb0973f7c0425b26faffe625ce90e076b
Zachary Smith <billyzacsmith@gmail.com>
2015-12-22 16:21:32 -0700

    Use 'node' for start command instead of 'nodemon'. Nodemon was restarting the server every time fs wrote to catalog.json, which made it impossible to handle requests.'

--- server.js
+++ server.js
@@ -48,55 +48,55 @@ app.get('/catalog', function(req, res) {
 })
 
 
-app.get('/catalog/:id', function(req, res) {
-  var id = Number(req.params.id);
-  for (var i = 0; i < db.catalog.length; i++) {
-    if (db.catalog[i].id === id) {
-      var item = db.catalog[i], i = db.catalog.length;
-    }
-  }
-  if (item) res.json(item);
-  else  res.sendStatus(404);
-});
+// app.get('/catalog/:id', function(req, res) {
+//   var id = Number(req.params.id);
+//   for (var i = 0; i < db.catalog.length; i++) {
+//     if (db.catalog[i].id === id) {
+//       var item = db.catalog[i], i = db.catalog.length;
+//     }
+//   }
+//   if (item) res.json(item);
+//   else  res.sendStatus(404);
+// });
 
-app.post('/catalog/new', function(req, res) {
-  var newid = db.catalog[db.catalog.length - 1].id + 1;
-  if (req.body.name && req.body.description) {
-    db.catalog.push({
-      id: newid,
-      name: req.body.name,
-      price: req.body.price,
-      description: req.body.description });
-    fs.writeFile('./fakedata.json', JSON.stringify(db), function(err) {
-      if (err) throw err;
-      else {
-        console.log('success')
-        res.send(true);
-      }
-    });
-  } else {
-    res.send(false);
-  }
-});
+// app.post('/catalog/new', function(req, res) {
+//   var newid = db.catalog[db.catalog.length - 1].id + 1;
+//   if (req.body.name && req.body.description) {
+//     db.catalog.push({
+//       id: newid,
+//       name: req.body.name,
+//       price: req.body.price,
+//       description: req.body.description });
+//     fs.writeFile('./fakedata.json', JSON.stringify(db), function(err) {
+//       if (err) throw err;
+//       else {
+//         console.log('success')
+//         res.send(true);
+//       }
+//     });
+//   } else {
+//     res.send(false);
+//   }
+// });
 
-app.delete('/catalog/:id', function(req, res) {
-  var id = Number(req.params.id);
-  var found = false;
-  for (var i = 0; i < db.catalog.length; i++) {
-    if (db.catalog[i].id === id) {
-      found = true;
-      db.catalog.splice(i, 1);
-      fs.writeFile('./fakedata.json', JSON.stringify(db), function(err) {
-        if (err) {
-          res.send(false);
-        }
-        else res.send(true);
-      });
-      i = db.catalog.length;
-    }
-  }
-  if (!found) res.send(false);
-});
+// app.delete('/catalog/:id', function(req, res) {
+//   var id = Number(req.params.id);
+//   var found = false;
+//   for (var i = 0; i < db.catalog.length; i++) {
+//     if (db.catalog[i].id === id) {
+//       found = true;
+//       db.catalog.splice(i, 1);
+//       fs.writeFile('./fakedata.json', JSON.stringify(db), function(err) {
+//         if (err) {
+//           res.send(false);
+//         }
+//         else res.send(true);
+//       });
+//       i = db.catalog.length;
+//     }
+//   }
+//   if (!found) res.send(false);
+// });
 
 app.listen(process.env.PORT || 8080);
 console.log('Server running on port ', process.env.PORT || 8080);
